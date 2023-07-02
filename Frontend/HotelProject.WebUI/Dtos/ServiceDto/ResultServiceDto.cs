a3cad07669ef11aec367cec33dc73eb581f2f9f4
imransaglam <imransaglam9@gmail.com>
2023-07-02 16:11:15 +0300

    Hizmetler kısmı DTO uygulanarak yapıldı

---
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/ResultServiceDto.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+    public class ResultServiceDto
+    {
+        //Listelemek için
+        public int ServiceID { get; set; }
+        public string ServiceIcon { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
