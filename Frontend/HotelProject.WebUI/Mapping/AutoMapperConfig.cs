c953664b1b301031720903f3344e9f78f3a3a12a
imransaglam <imransaglam9@gmail.com>
2023-07-04 13:08:36 +0300

    anasayfadaki team kısmı yapıldı

--- Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
+++ Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
@@ -4,6 +4,8 @@ using HotelProject.WebUI.Dtos.AboutDto;
 using HotelProject.WebUI.Dtos.LoginDto;
 using HotelProject.WebUI.Dtos.RegisterDto;
 using HotelProject.WebUI.Dtos.ServiceDto;
+using HotelProject.WebUI.Dtos.StaffDto;
+using HotelProject.WebUI.Dtos.TestimonialDto;
 
 namespace HotelProject.WebUI.Mapping
 {
@@ -14,10 +16,16 @@ namespace HotelProject.WebUI.Mapping
             CreateMap<ResultServiceDto, Service>().ReverseMap();
             CreateMap<CreateServiceDto, Service>().ReverseMap();
             CreateMap<UpdateServiceDto, Service>().ReverseMap();
-            CreateMap<CreateNewUserDto, Service>().ReverseMap();
-            CreateMap<LoginUserDto, Service>().ReverseMap();
-            CreateMap<ResultAboutDto, Service>().ReverseMap();
-            CreateMap<UpdateAboutDto,Service>().ReverseMap();   
+
+            CreateMap<CreateNewUserDto, AppUser>().ReverseMap();
+            CreateMap<LoginUserDto, AppUser>().ReverseMap();
+
+            CreateMap<ResultAboutDto, About>().ReverseMap();
+            CreateMap<UpdateAboutDto,About>().ReverseMap();
+
+            CreateMap<ResultTestimonialDto, Testimonial>().ReverseMap();   
+
+            CreateMap<ResultStaffDto, Staff>().ReverseMap();
 
         }
  
