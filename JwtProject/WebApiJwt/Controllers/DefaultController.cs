6e945bdd07dfeb16485583eea2c3c0578c0e40d7
imransaglam <imransaglam9@gmail.com>
2023-07-06 15:39:28 +0300

    json web token(jwt) uzerine calısıldı

---
+++ JwtProject/WebApiJwt/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiJwt.Models;
+
+namespace WebApiJwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        //http://localhost:5094/api/Default/Test token oluşturdu oluşan tokenı jwt.io adresinde kopyala ve decoded et
+        [HttpGet("[action]")]   
+        public IActionResult TokenOlustur() 
+        {
+            return Ok(new CreateToken().TokenCreate());
+        }
+        [HttpGet("[action]")]
+        public IActionResult AdminTokenOlustur()
+        {
+            return Ok(new CreateToken().TokenCreateAdmin());
+        }
+
+        [Authorize]
+        [HttpGet("[action]")] 
+        public IActionResult Test2()
+        {
+            return Ok("Hoş Geldiniz");
+        }
+
+        [Authorize(Roles ="Admin,Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult Test3()
+        {
+            return Ok("Token Başarılı bir şekilde giriş yaptı");
+        }
+    }
+}
---
+++ JwtProject/WebApiJwt/Models/CreateToken.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace WebApiJwt.Models
+{
+    //Token oluşturmak
+    public class CreateToken
+    {
+        public string TokenCreate() 
+        {
+            var bytes = Encoding.UTF8.GetBytes("aspnetcoreapiapi");
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+            SigningCredentials credentials=new SigningCredentials(key,SecurityAlgorithms.HmacSha256);//HmacSha256=şifreleme algoritması 
+            JwtSecurityToken token = new JwtSecurityToken(issuer:"http//localhost",audience:"http//localhost",
+                notBefore:DateTime.Now,expires:DateTime.Now.AddMinutes(5),signingCredentials:credentials);//notbefore:neyden önce(DateTime.Now=önceden oluşturmuş olmalı),expires:tokenın geçerlilik süresi
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();//oluşturucu
+            return handler.WriteToken(token);//token yaz
+
+            //claim rollerin içeriği
+        
+        }
+        public string TokenCreateAdmin() 
+        {
+            var bytes = Encoding.UTF8.GetBytes("aspnetcoreapiapi");
+            SymmetricSecurityKey key=new SymmetricSecurityKey(bytes);
+            SigningCredentials credentials = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier,Guid.NewGuid().ToString()),//name'in id değeri oluyor guid sayesinde
+                new Claim(ClaimTypes.Role,"Admin"),
+                new Claim(ClaimTypes.Role,"Visitor")
+            };
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost",
+                notBefore:DateTime.Now,expires:DateTime.Now.AddSeconds(30),signingCredentials:credentials,claims:claims);
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(jwtSecurityToken);
+        }
+    }
+}
